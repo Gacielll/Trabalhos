b881140ecf310f0aceeb2507124ee9cfd245b839
Gacielll <gabriel.maciel3642@gmail.com>
2025-03-26 19:06:16 -0300

    Frações

--- main.c
+++ main.c
@@ -1,43 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-//Codigo para calcular frações(somar, subtrair, dividir, multiplicar)
+//Codigo para calcular fraÃ§Ãµes(somar, subtrair, dividir, multiplicar)
 void obterfracoes (int *num1, int *num2, int *num3, int *num4)
 {
-    printf("Informe O numerador da primeira fração:\n");
+    printf("Informe O numerador da primeira fraÃ§Ã£o:\n");
     scanf("%d", num1);
-    printf("\nInforme o denominador da primeira fração:\n");
+    printf("\nInforme o denominador da primeira fraÃ§Ã£o:\n");
     scanf("%d", num2);
-    printf("\nInforme o numerador da segunda fração:\n");
+    printf("\nInforme o numerador da segunda fraÃ§Ã£o:\n");
     scanf("%d", num3);
-    printf("\nInforme o denominador da segunda fração:\n");
+    printf("\nInforme o denominador da segunda fraÃ§Ã£o:\n");
     scanf("%d", num4);
 
     if(*num2 == 0 || *num4 == 0)//Caso o usuario escrever 0 no denominador
     {
-        printf("\nNão é possível ter uma fração com denominador zero, pois a divisão por zero é indefinida. ");
+        printf("\nNÃ£o Ã© possÃ­vel ter uma fraÃ§Ã£o com denominador zero, pois a divisÃ£o por zero Ã© indefinida. ");
         exit(1);//Finaliza o programa retornando o numero 1 para o int main
     }
 }
-//soma das frações
+//soma das fraÃ§Ãµes
 void somarfra(int num1, int num2, int num3, int num4, int *num, int *den)
 {
     *num = num1 * num4 + num3 * num2;
     *den = num2 * num4;
 }
-//subtração das frações
+//subtraÃ§Ã£o das fraÃ§Ãµes
 void subtrairfra(int num1, int num2, int num3, int num4, int *num, int *den)
 {
     *num = num1 * num4 - num3 * num2;
     *den = num2 * num4;
 }
-//multiplicação das frações
+//multiplicaÃ§Ã£o das fraÃ§Ãµes
 void multiplicarfra(int num1, int num2, int num3, int num4, int *num, int *den)
 {
     *num = num1 * num3;
     *den = num2 * num4;
 }
-//divisão das frações
+//divisÃ£o das fraÃ§Ãµes
 void dividirfra(int num1, int num2, int num3, int num4, int *num, int *den)
 {
     *num = num1 * num4;
@@ -61,12 +61,11 @@ void mdc(int *num, int *den)
 int main()
 {
     int numA, numB, numC, numD, numres, denres, opcao;
-    char resp;
+    char resp, continuar;
 
     setlocale( LC_ALL, "portuguese");//Torna possivel utilizar acentos
     do
     {
-        system("cls");//Limpa a tela
         printf("1-Somar!\n");
         printf("2-Subtrair!\n");
         printf("3-Multiplicar!\n");
@@ -83,35 +82,45 @@ int main()
             obterfracoes(&numA, &numB, &numC, &numD);
             somarfra(numA, numB, numC, numD, &numres, &denres);
             mdc(&numres, &denres);
-            printf("O resultado é: %d/%d\n", numres, denres);
+            printf("O resultado Ã©: %d/%d\n", numres, denres);
             break;
         case 2:
             obterfracoes(&numA, &numB, &numC, &numD);
             subtrairfra(numA, numB, numC, numD, &numres, &denres);
             mdc(&numres, &denres);
-            printf("O resultado é: %d/%d\n", numres, denres);
+            printf("O resultado Ã©: %d/%d\n", numres, denres);
             break;
         case 3:
             obterfracoes(&numA, &numB, &numC, &numD);
             multiplicarfra(numA, numB, numC, numD, &numres, &denres);
             mdc(&numres, &denres);
-            printf("O resultado é: %d/%d\n", numres, denres);
+            printf("O resultado Ã©: %d/%d\n", numres, denres);
             break;
         case 4:
             obterfracoes(&numA, &numB, &numC, &numD);
             dividirfra(numA, numB, numC, numD, &numres, &denres);
             mdc(&numres, &denres);
-            printf("O resultado é: %d/%d\n", numres, denres);
+            printf("O resultado Ã©: %d/%d\n", numres, denres);
             break;
         case 5:
             printf("Encerrando programa");
             exit(1);
         default:
-            printf("Opção invalida\n");
+            printf("OpÃ§Ã£o invalida\n");
             break;
         }
+
+        printf("Gostaria de tentar outro calculo? s/n\n");
+        {
+            scanf(" %c", &continuar);
+        }
+        if(continuar == 'n')
+        {
+            exit(1);
+        }
+
     }
-    while(opcao != 5);
+    while(continuar == 's');
 
     return 0;
 }
